110eb137c990dcb399b53c4c0b66e3459bfaa570
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-04-12 12:09:41 -0300

    Correção de mensagens de erro, igualdade e aprovação

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 console.log ("JavaScript Carregado com Sucesso"); //confirmação de carregamento JS
 
 const form = document.querySelector('#validacaoValorMaior');
+const mensagemValorIgual = document.getElementById('valorIgual');
+const mensagemValorMaior = document.getElementById('mensagemValorAMaior');
+const mensagemValorCorreto = document.getElementById('valorCorreto');
 
 form.onsubmit = evento => {
     //Receber o valor do campo A e B
@@ -10,7 +13,8 @@ form.onsubmit = evento => {
     //Verificação de valor maior
     if (valorA > valorB) {
         evento.preventDefault ();
-        document.getElementById('alertMessege').innerHTML = "<h3>O valor de A é maior que B.<p>Tente Novamente!</p>";
+        mensagemValorMaior.innerHTML = "O valor de A é maior que B. Tente novamente";
+        mensagemValorMaior.style.display = 'block';
     
     //Validação de recebimento de valores A e B
     console.log ("O valor",valorA,"referente a A é maior que o valor",valorB,"referente a B. Tente novamente!");
@@ -18,14 +22,20 @@ form.onsubmit = evento => {
     }
     else if (valorB === valorA) {
         evento.preventDefault ();
-        document.getElementById('alertMessege').innerHTML = "<h3>Os valores A e B são identicos.<p>Insira valores diferentes entre A e B</p>";
+        mensagemValorIgual.innerHTML = "O valor de A e B são identicos, insira valores diferentes para A e B";
+        mensagemValorIgual.style.display = 'block';
     
     //Validação de recebimento de valores A e B
     console.log ("O valor",valorA,"referente a A e o valor",valorB,"referente a B são identicos. Insira valores diferentes entre A e B!");
         return;
     }
     else {
-        alert("Parabéns, o valor de B é maior que o de A");
+        evento.preventDefault ();
+        mensagemValorCorreto.innerHTML = "Boa!! O valor de B é maior que A.";
+        mensagemValorCorreto.style.display = 'block';
+
+        //Validação de recebimento de valores A e B
+        console.log ("Parabéns! O valor de",valorB,"referente a B é maior que o valor",valorA,"referente a A.");
         return;
     }
 
