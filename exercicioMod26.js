98ff46f05ab81259b2291f3166ac5f4a0d4a03bf
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-06-14 10:38:40 -0300

    Resolução Exercicio Mod 26

---
+++ exercicioMod26.js
@@ -0,0 +1,11 @@
+"use strict";
+function calculandoValor(valor1, valor2) {
+    return valor1 * valor2;
+}
+;
+console.log(calculandoValor(500, 6));
+function saudacao(nome) {
+    return 'Olá ' + nome + ', tudo bem com você?';
+}
+;
+console.log(saudacao('tutor da EBAC'));
---
+++ exercicioMod26.ts
@@ -0,0 +1,9 @@
+function calculandoValor (valor1 : number, valor2 : number) : number{
+    return valor1 * valor2
+};
+console.log(calculandoValor(500,6));
+
+function saudacao (nome : string) : string{
+    return 'Olá ' + nome + ', tudo bem com você?'
+};
+console.log(saudacao('tutor da EBAC'));
